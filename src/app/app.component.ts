ef0b9925a3bcbb8eb16eabf0801d0e231361f1de
dariathehuman <nabatova.darya@protonmail.com>
2020-10-26 15:45:38 +0300

    layout shopping-list and a little bit of logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipebook';
+  title = 'Recipe Book';
+  loadedFeature = 'recipes';
+
+  onNavigate(feature) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient [] = [
+    new Ingredient('Apples', 10),
+    new Ingredient('Oranges', 5),
+    new Ingredient('Tomatoes', 2)
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
 
 }
