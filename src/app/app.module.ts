2fcbee041bf8544638f32bd0771196ead8925b0d
dariathehuman <nabatova.darya@protonmail.com>
2021-01-26 20:39:30 +0300

    added lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -9,12 +8,8 @@ import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesService } from './recipes/recipes.service';
-import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from './auth/auth.interceptor.service';
-import {RecipesModule} from './recipes/recipes.module';
-import {IconsModule} from './modules/icons/icons.module';
-import {ShoppingListModule} from './shopping-list/shopping-list.module';
-import {AuthModule} from './auth/auth.module';
+import { AuthInterceptorService } from './auth/auth.interceptor.service';
+import { IconsModule } from './modules/icons/icons.module';
 
 @NgModule({
   declarations: [
@@ -26,10 +21,6 @@ import {AuthModule} from './auth/auth.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    RecipesModule,
-    ShoppingListModule,
-    AuthModule,
     IconsModule
   ],
   providers: [ShoppingListService, RecipesService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
